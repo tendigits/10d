1f64b37f3049799361117fa6ca3704497bbd1388
Nate Steiner <nsteiner@gmail.com>
2021-05-14 09:47:02 -0400

    customized macro template for tendigits

--- src/macro.sh
+++ src/macro.sh
@@ -4,9 +4,10 @@
 # ./macro.sh term1 term2 term3
 
 INC='./inc/'
-TPL='<h2>Title Goes Here</h2>'
+TPL1='<header>\r\t<p class="pretitle"></p>\r\t<h1>'
+TPL2='</h1>\r\t<p class="posttitle"></p>\r</header>\r<main>\r</main>'
 
 for var in "$@"
 do
-	echo -e $TPL >> $INC$var'.htm'
+	echo -e $TPL1$var$TPL2 >> $INC$var'.htm'
 done
