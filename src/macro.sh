5ae792986847a860769622cd3c066d68f79feb8f
Nate Steiner <nsteiner@gmail.com>
2021-05-21 10:03:43 -0400

    optimized images, new granola recipe

--- src/macro.sh
+++ src/macro.sh
@@ -5,9 +5,11 @@
 
 INC='./inc/'
 TPL1='<header>\r\t<p class="pretitle"></p>\r\t<h1>'
-TPL2='</h1>\r\t<p class="posttitle"></p>\r</header>\r<main>\r</main>'
+TPL2='</h1>\r\t<p class="posttitle"></p>\r</header>\r<main>'
+TPL3='<figure>\r\t<a href="#imghigh" class="h">•</a><a href="#imglow" class="l">•</a>\r\t<img class="high" src="../assets/img/" width="" height="" alt="" loading="lazy">\r\t<img class="low" src="../assets/img/" width="" height="" alt="" loading="lazy">\r\t<figcaption></figcaption>\r</figure>'
+TPL4='\r</main>'
 
 for var in "$@"
 do
-	echo -e $TPL1$var$TPL2 >> $INC$var'.htm'
+	echo -e $TPL1$var$TPL2$TPL3$TPL4 >> $INC$var'.htm'
 done
