34bde4cfd4259a4e383a64bfb56b93552d7c35aa
Nate Steiner <nsteiner@gmail.com>
2021-05-21 16:21:10 -0400

    new image swapper button style

--- src/macro.sh
+++ src/macro.sh
@@ -6,7 +6,7 @@
 INC='./inc/'
 TPL1='<header>\r\t<p class="pretitle"></p>\r\t<h1>'
 TPL2='</h1>\r\t<p class="posttitle"></p>\r</header>\r<main>'
-TPL3='<figure>\r\t<a href="#imghigh" class="h">•</a><a href="#imglow" class="l">•</a>\r\t<img class="high" src="../assets/img/" width="" height="" alt="" loading="lazy">\r\t<img class="low" src="../assets/img/" width="" height="" alt="" loading="lazy">\r\t<figcaption></figcaption>\r</figure>'
+TPL3='<figure>\r\t<a href="#imghigh" class="h">More Pixels</a><a href="#imglow" class="l">Fewer Pixels</a>\r\t<img class="high" src="../assets/img/" width="" height="" alt="" loading="lazy">\r\t<img class="low" src="../assets/img/" width="" height="" alt="" loading="lazy">\r\t<figcaption></figcaption>\r</figure>'
 TPL4='\r</main>'
 
 for var in "$@"
