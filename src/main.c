ba37f7d18567271f6c603e5dff92df5c3a7ae381
nsteiner <nsteiner@gmail.com>
2021-08-12 15:33:58 -0400

    fixed feed path in header

--- src/main.c
+++ src/main.c
@@ -163,7 +163,7 @@ build(FILE *f, Lexicon *l, char *name, char *srcpath)
 		"<meta name='description' content='%s'/>"
 		"<meta name='viewport' content='width=device-width,initial-scale=1'>"
 		"<link rel='alternate' type='application/rss+xml' title='RSS Feed' "
-		"href='../feed.xml' />"
+		"href='https://tendigits.space/feed.xml' />"
 		"<link rel='stylesheet' type='text/css' href='../links/main.css?c=20210721'>"
 		"<title>" NAME " &mdash; %s</title>"
 		"<link rel='shortcut icon' type='image/png' href='/favicon.ico'>",
