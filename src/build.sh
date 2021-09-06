0c1bf25bb99dd87fa522084cf346ed54f9f65c7e
Nate Steiner <nsteiner@gmail.com>
2021-09-06 14:03:42 -0400

    bagel recipe update

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # START SPEED TEST
-res1=$(date +%s.%N)
+#res1=$(date +%s.%N)
 # Lint
 # clang-format -i main.c
 
@@ -34,13 +34,13 @@ mkdir ../site
 # Cleanup
 rm -f ./main
 # FINISH SPEED TEST
-res2=$(date +%s.%N)
-dt=$(echo "$res2 - $res1" | bc)
-dd=$(echo "$dt/86400" | bc)
-dt2=$(echo "$dt-86400*$dd" | bc)
-dh=$(echo "$dt2/3600" | bc)
-dt3=$(echo "$dt2-3600*$dh" | bc)
-dm=$(echo "$dt3/60" | bc)
-ds=$(echo "$dt3-60*$dm" | bc)
+#res2=$(date +%s.%N)
+#dt=$(echo "$res2 - $res1" | bc)
+#dd=$(echo "$dt/86400" | bc)
+#dt2=$(echo "$dt-86400*$dd" | bc)
+#dh=$(echo "$dt2/3600" | bc)
+#dt3=$(echo "$dt2-3600*$dh" | bc)
+#dm=$(echo "$dt3/60" | bc)
+#ds=$(echo "$dt3-60*$dm" | bc)
 
-LC_NUMERIC=C printf "Total runtime: %d:%02d:%02d:%02.4f\n" $dd $dh $dm $ds
+#LC_NUMERIC=C printf "Total runtime: %d:%02d:%02d:%02.4f\n" $dd $dh $dm $ds
